a978758becec820197c030fc501deaab0eda0ccb
Wilson Marques <wilsonmarquesg12@gmail.com>
2020-03-16 15:35:40 -0400

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/wilsonmarques/cursomc/cursomc/CursomcApplication.java
+++ src/main/java/com/wilsonmarques/cursomc/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.wilsonmarques.cursomc.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import com.wilsonmarques.cursomc.cursomc.domain.Categoria;
@@ -7,13 +8,20 @@ import com.wilsonmarques.cursomc.cursomc.domain.Cidade;
 import com.wilsonmarques.cursomc.cursomc.domain.Cliente;
 import com.wilsonmarques.cursomc.cursomc.domain.Endereco;
 import com.wilsonmarques.cursomc.cursomc.domain.Estado;
+import com.wilsonmarques.cursomc.cursomc.domain.Pagamento;
+import com.wilsonmarques.cursomc.cursomc.domain.PagamentoComBoleto;
+import com.wilsonmarques.cursomc.cursomc.domain.PagamentoComCartao;
+import com.wilsonmarques.cursomc.cursomc.domain.Pedido;
 import com.wilsonmarques.cursomc.cursomc.domain.Produto;
+import com.wilsonmarques.cursomc.cursomc.domain.enums.EstadoPagamento;
 import com.wilsonmarques.cursomc.cursomc.domain.enums.TipoCliente;
 import com.wilsonmarques.cursomc.cursomc.repositories.CategoriaRepository;
 import com.wilsonmarques.cursomc.cursomc.repositories.CidadeRepository;
 import com.wilsonmarques.cursomc.cursomc.repositories.ClienteRepository;
 import com.wilsonmarques.cursomc.cursomc.repositories.EnderecoRepository;
 import com.wilsonmarques.cursomc.cursomc.repositories.EstadoRepository;
+import com.wilsonmarques.cursomc.cursomc.repositories.PagamentoRepository;
+import com.wilsonmarques.cursomc.cursomc.repositories.PedidoRepository;
 import com.wilsonmarques.cursomc.cursomc.repositories.ProdutoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +49,12 @@ public class CursomcApplication implements CommandLineRunner{
 
 	@Autowired
 	EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	PedidoRepository pedidoRepository;
+
+	@Autowired
+	PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -89,6 +103,21 @@ public class CursomcApplication implements CommandLineRunner{
 
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 
 	}
 
