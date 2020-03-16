86d3607ad5276e2ab79bbe6ba059b9c6b63ae5b3
Wilson Marques <wilsonmarquesg12@gmail.com>
2020-03-16 19:36:59 -0400

    Endpoin /pedidos/{id} disponibilizado

---
+++ src/main/java/com/wilsonmarques/cursomc/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.wilsonmarques.cursomc.cursomc.services;
+
+import java.util.Optional;
+
+import com.wilsonmarques.cursomc.cursomc.domain.Pedido;
+import com.wilsonmarques.cursomc.cursomc.repositories.PedidoRepository;
+import com.wilsonmarques.cursomc.cursomc.services.exceptions.ObjectNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PedidoService {
+
+    @Autowired
+    private PedidoRepository repo;
+
+    public Pedido buscar(Integer id){
+        Optional<Pedido> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+            "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+    }
+
+}
